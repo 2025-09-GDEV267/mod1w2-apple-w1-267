4a13b4254558758530db68c9df1bad71165e2984
4RVCC30 <G00300532@stu.raritanval.edu>
2025-09-11 12:46:37 -0400

    Some more stuff
    uwu

--- Assets/TreeScript.cs
+++ Assets/TreeScript.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class TreeScript : MonoBehaviour
 {
     const int RANGE = 5;
+    [SerializeField]
+    GameObject Apple;
     public void moveTree()
     {
         transform.position = new Vector3(Random.Range(-RANGE, RANGE), transform.position.y, transform.position.z);
+        Instantiate(Apple);
     }
 }
