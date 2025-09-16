f75bb5838cf2d99e151f874ccb7a76454a9a2728
kstrong03 <kat.strong2003@gmail.com>
2025-09-16 13:40:11 -0400

    tree now randomly spits out apples

--- Assets/scripts/AppleTree.cs
+++ Assets/scripts/AppleTree.cs
@@ -22,6 +22,13 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         // Dropping apples every second
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
     }
 
     void Update()
