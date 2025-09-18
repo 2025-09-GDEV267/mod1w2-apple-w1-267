3c6ecc8ba3e4de836f271869c097446273f30c4b
kstrong03 <kat.strong2003@gmail.com>
2025-09-18 13:23:06 -0400

    high score now updates when broken

---
+++ Assets/scripts/HighScore.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+    }
+
+
+}
