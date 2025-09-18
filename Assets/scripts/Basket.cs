474d79d491b35e7fbeef4667fee5037bc41b8cdb
kstrong03 <kat.strong2003@gmail.com>
2025-09-18 13:12:08 -0400

    the score on the right hand side now changes when apples are dropped into the basket

--- Assets/scripts/Basket.cs
+++ Assets/scripts/Basket.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+
+    public Text        scoreGT;
+
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");               // 2
+        // Get the GUIText Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();                          // 3
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
 
     void Update()
     {
@@ -29,5 +42,12 @@ public class Basket : MonoBehaviour
         {                                // 4
             Destroy(collidedWith);
         }
+
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);                              // 4
+        // Add points for catching the apple
+        score += 100;
+        // Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
     }
 }
