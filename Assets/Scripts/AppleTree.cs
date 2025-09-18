e52427e1409977ad10a5a7eddfcc5de38cf8d57b
Sentientprism <doughertyedmund7@gmail.com>
2025-09-17 20:30:24 -0400

    tree movement

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    [Header("Inscribed")]                                                 
+    [Header("Inscribed")]
 
-     public GameObject applePrefab;
-     public float speed = 1f;
-     public float leftAndRightEdge = 10f;
-     public float changeDirChance = 0.1f;
-     public float appleDropDelay = 1f;
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float changeDirChance = 0.1f;
+    public float appleDropDelay = 1f;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+        else if (Random.value < changeDirChance)
+        {
+            speed *= -1;
+
+
+        }
     }
 }
